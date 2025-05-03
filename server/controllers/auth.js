640325b2248fd45895329f5caa147bfb7150f00d
betingrich4 <mariseltru@gmail.com>
2025-05-04 01:23:59 +0300

    Create auth.js

---
+++ server/controllers/auth.js
@@ -0,0 +1,54 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { JWT_SECRET } = process.env;
+
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username or email already exists' });
+    }
+    
+    const user = new User({ username, email, password });
+    await user.save();
+    
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+    
+    res.status(201).json({ token, user: { id: user._id, username, email } });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid credentials' });
+    }
+    
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid credentials' });
+    }
+    
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+    
+    res.json({ token, user: { id: user._id, username: user.username, email } });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
